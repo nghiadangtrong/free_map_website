facb7b6d1dcfc604edf1cd9b1658f48b40c6fbe7
nghia dang <nghiadangtrongn92@gmail.com>
2018-04-03 23:16:43 +0700

    update

---
+++ website/sublimeText.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1522709779587" ID="ID_1579372905" MODIFIED="1522709787979" TEXT="sublime text">
+<node CREATED="1522709789077" ID="ID_1961726857" MODIFIED="1522709793955" POSITION="right" TEXT="install"/>
+<node CREATED="1522709794402" ID="ID_1195546322" MODIFIED="1522709854122" POSITION="right" TEXT="setting key">
+<node CREATED="1522709855070" ID="ID_1292140668" MODIFIED="1522709883940" TEXT="Preferences -&gt; key binding">
+<node CREATED="1522709903427" ID="ID_617024468" MODIFIED="1522709905706" TEXT="{ &quot;keys&quot;: [&quot;f12&quot;], &quot;command&quot;: &quot;toggle_side_bar&quot; }"/>
+<node CREATED="1522709916941" ID="ID_1251277957" MODIFIED="1522709919052" TEXT="{ &quot;keys&quot;: [&quot;ctrl+shift+o&quot;], &quot;command&quot;: &quot;prompt_open_folder&quot;}, "/>
+</node>
+</node>
+</node>
+</map>
