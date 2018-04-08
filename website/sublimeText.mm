0e619b6738b35b5531dd5decbb428f93e1843aa6
nghia dang <nghiadangtrongn92@gmail.com>
2018-04-08 23:56:05 +0700

    add: sublimetext; edit: laravel

--- website/sublimeText.mm
+++ website/sublimeText.mm
@@ -5,7 +5,7 @@
 <node CREATED="1522709794402" ID="ID_1195546322" MODIFIED="1522709854122" POSITION="right" TEXT="setting key">
 <node CREATED="1522709855070" ID="ID_1292140668" MODIFIED="1522709883940" TEXT="Preferences -&gt; key binding">
 <node CREATED="1522709903427" ID="ID_617024468" MODIFIED="1522709905706" TEXT="{ &quot;keys&quot;: [&quot;f12&quot;], &quot;command&quot;: &quot;toggle_side_bar&quot; }"/>
-<node CREATED="1522709916941" ID="ID_1251277957" MODIFIED="1522709919052" TEXT="{ &quot;keys&quot;: [&quot;ctrl+shift+o&quot;], &quot;command&quot;: &quot;prompt_open_folder&quot;}, "/>
+<node COLOR="#000000" CREATED="1522709916941" ID="ID_1251277957" MODIFIED="1523040018504" TEXT="{ &quot;keys&quot;: [&quot;ctrl+shift+o&quot;], &quot;command&quot;: &quot;prompt_open_folder&quot;}, "/>
 </node>
 </node>
 </node>
