d3fa0d82483cdaf4292fcb79681d932c6f360f37
Unknown <nghiadangtrongn92@gmail.com>
2018-07-31 23:08:17 +0700

    update

---
+++ website/Mysql.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1533052052977" ID="ID_355795554" MODIFIED="1533052061380" TEXT="Mysql">
+<node CREATED="1533052063591" ID="ID_930299374" MODIFIED="1533052070021" POSITION="right" TEXT="install"/>
+<node CREATED="1533052159886" ID="ID_1658005896" MODIFIED="1533052161478" POSITION="right" TEXT="config"/>
+<node CREATED="1533052161938" ID="ID_763081054" MODIFIED="1533052172650" POSITION="right" TEXT="database">
+<node CREATED="1533052173714" ID="ID_318545772" MODIFIED="1533052183208" TEXT="create database name;"/>
+<node CREATED="1533052183697" ID="ID_475577669" MODIFIED="1533052197317" TEXT="drop database name;"/>
+</node>
+</node>
+</map>
