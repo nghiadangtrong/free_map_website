5d4e822de97a69000e8f23551cb086266a36bf6b
nghia <dang.trong.nghia@mqsolutions.com.vn>
2018-09-18 15:44:23 +0700

    update
    javasript

---
+++ website/javascript ES6,7.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1537251095689" ID="ID_1180108946" MODIFIED="1537251112145" TEXT="javascript ES6,7">
+<node CREATED="1537251220783" ID="ID_1360040855" MODIFIED="1537251259819" POSITION="right" TEXT="sync / async">
+<node CREATED="1537251265039" ID="ID_191070280" MODIFIED="1537251268562" TEXT="callback"/>
+<node CREATED="1537251268976" ID="ID_374824369" MODIFIED="1537251272519" TEXT="promise"/>
+<node CREATED="1537251272800" ID="ID_737441083" MODIFIED="1537251281802" TEXT="async / await"/>
+</node>
+</node>
+</map>
