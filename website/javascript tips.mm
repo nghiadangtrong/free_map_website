57e447e8a35fe7d57fcf4a4fb2cf2d7f5121665b
nghiadangtrong <nghiadangtrongn92@gmail.com>
2018-09-26 23:47:57 +0700

    add tips javascripts

---
+++ website/javascript tips.mm
@@ -0,0 +1,28 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1537976922239" ID="ID_1492318846" MODIFIED="1537976936696" TEXT="Trick javascript">
+<node CREATED="1537976937581" ID="ID_1448014268" MODIFIED="1537976939974" POSITION="right" TEXT="console">
+<node CREATED="1537978832562" ID="ID_1352239443" MODIFIED="1537978836665" TEXT="console.log">
+<node CREATED="1537978840327" ID="ID_811261438" MODIFIED="1537978849957" TEXT="%s, %i, %o, %f"/>
+</node>
+<node CREATED="1537976940890" ID="ID_1740296075" MODIFIED="1537977045033" TEXT="console.dir() : show to object"/>
+<node CREATED="1537977309738" ID="ID_288430756" MODIFIED="1537978407184" TEXT="console.trace(value/ function/ element/ ... all): show v&#x1ecb; tr&#xed; c&#x169;ng nh&#x1b0; t&#xea;n code"/>
+<node CREATED="1537977893274" ID="ID_1313163005" MODIFIED="1537977898394" TEXT="time">
+<node CREATED="1537977899873" ID="ID_838560096" MODIFIED="1537977913919" TEXT="console.time(&apos;value_name&apos;)"/>
+<node CREATED="1537977914242" ID="ID_1325433691" MODIFIED="1537977931941" TEXT="console.timeEnd(&apos;value_name&apos;)"/>
+<node CREATED="1537977932818" ID="ID_508649627" MODIFIED="1537977943394" TEXT="==&gt; show : value_name: value"/>
+</node>
+<node CREATED="1537978391010" ID="ID_5353969" MODIFIED="1537978436959" TEXT="console.memory">
+<node CREATED="1537978438353" ID="ID_1057882544" MODIFIED="1537978451723" TEXT="Show detail memory use for browser"/>
+</node>
+<node CREATED="1537978676104" ID="ID_1767561190" MODIFIED="1537978694095" TEXT="console.count(&apos;name_set&apos;)">
+<node CREATED="1537978696022" ID="ID_794665551" MODIFIED="1537978739914" TEXT="T&#xe1;c dung: c&#xf3; th&#x1ec3; &#x111;&#x1ebf;m s&#x1ed1; l&#x1ea7;n d&#xf2;ng code th&#x1ef1;c hi&#x1ec7;n cho debug">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1537977273467" ID="ID_1463877188" MODIFIED="1537977286760" POSITION="right" TEXT="debugger;">
+<node CREATED="1537977287932" ID="ID_1431600829" MODIFIED="1537977306026" TEXT="set break point"/>
+</node>
+</node>
+</map>
