69f36d462c4997c656b431545d89db742cae9b72
nghiadangtrong <nghiadangtrongn92@gmail.com>
2019-08-09 01:12:30 +0700

    udpate

--- website/Laravel-Complete.mm
+++ website/Laravel-Complete.mm
@@ -1,9 +1,26 @@
-<map version="1.0.1">
+<map version="1.1.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1565236547703" ID="ID_353104198" MODIFIED="1565236634213" TEXT="Laravel-Complete">
-<node CREATED="1565236754418" ID="ID_1212256967" MODIFIED="1565236831852" POSITION="right" TEXT="start">
-<node CREATED="1565236834306" ID="ID_1093875646" MODIFIED="1565236843513" TEXT="https://getcomposer.org/download/">
-<node CREATED="1565236845033" ID="ID_1883294949" MODIFIED="1565236850445" TEXT="download composer"/>
+<node CREATED="1565236754418" ID="ID_1212256967" MODIFIED="1565286792282" POSITION="right" TEXT="start">
+<node CREATED="1565286792235" ID="ID_951248650" MODIFIED="1565286814427" TEXT="isntall composer">
+<node CREATED="1565286814416" ID="ID_257127342" MODIFIED="1565286817403" TEXT="windown">
+<node CREATED="1565236834306" ID="ID_1093875646" MODIFIED="1565236843513" TEXT="https://getcomposer.org/download/"/>
+</node>
+<node CREATED="1565286817883" ID="ID_1412235990" MODIFIED="1565286820052" TEXT="ubuntu">
+<node CREATED="1565286824854" ID="ID_565496811" MODIFIED="1565286828126" TEXT="sudo curl -s https://getcomposer.org/installer | /opt/lampp/bin/php ">
+<node CREATED="1565286847784" ID="ID_55653221" MODIFIED="1565286869937" TEXT="download v&#xe0; run composer b&#x1eb1;ng php"/>
+</node>
+<node CREATED="1565286871593" ID="ID_608704697" MODIFIED="1565286982547" TEXT="sudo ln -s /opt/lampp/bin/php /usr/local/bin/php ">
+<node CREATED="1565286881304" ID="ID_423277931" MODIFIED="1565286923666" TEXT="Li&#xea;n k&#x1ebf;t file "/>
+<node CREATED="1565286923920" ID="ID_361047110" MODIFIED="1565286954458" TEXT="S&#x1eed; d&#x1ee5;ng php command global"/>
+</node>
+<node CREATED="1565286983868" ID="ID_101967523" MODIFIED="1565286985573" TEXT="sudo mv composer.phar /usr/local/bin/composer ">
+<node CREATED="1565286985890" ID="ID_1952672154" MODIFIED="1565287023349" TEXT="S&#x1eed; d&#x1ee5;ng composer command line global"/>
+</node>
+<node CREATED="1565287027968" ID="ID_448655215" MODIFIED="1565287039830" TEXT="composer --version ">
+<node CREATED="1565287040389" ID="ID_672741268" MODIFIED="1565287044847" TEXT="test"/>
+</node>
+</node>
 </node>
 <node CREATED="1565236767620" ID="ID_1963802184" MODIFIED="1565236821053" TEXT="composer create-project laravel/laravel &lt;name_project&gt; dev-develop">
 <node CREATED="1565236822124" ID="ID_1631320838" MODIFIED="1565236830076" TEXT="Kh&#x1edf;i t&#x1ea1;o project"/>
