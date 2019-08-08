88920b15e1ae3233452d9dea6b5dd1bc6514c10c
nghiadangtrong <dang.trong.nghia@mqsolutions.com.vn>
2019-08-08 17:01:16 +0700

    update laravel

---
+++ website/Laravel-Complete.mm
@@ -0,0 +1,19 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1565236547703" ID="ID_353104198" MODIFIED="1565236634213" TEXT="Laravel-Complete">
+<node CREATED="1565236754418" ID="ID_1212256967" MODIFIED="1565236831852" POSITION="right" TEXT="start">
+<node CREATED="1565236834306" ID="ID_1093875646" MODIFIED="1565236843513" TEXT="https://getcomposer.org/download/">
+<node CREATED="1565236845033" ID="ID_1883294949" MODIFIED="1565236850445" TEXT="download composer"/>
+</node>
+<node CREATED="1565236767620" ID="ID_1963802184" MODIFIED="1565236821053" TEXT="composer create-project laravel/laravel &lt;name_project&gt; dev-develop">
+<node CREATED="1565236822124" ID="ID_1631320838" MODIFIED="1565236830076" TEXT="Kh&#x1edf;i t&#x1ea1;o project"/>
+</node>
+<node CREATED="1565236851865" ID="ID_1055017142" MODIFIED="1565236886501" TEXT="php artisan key:generate">
+<node CREATED="1565236888707" ID="ID_1298113753" MODIFIED="1565236907933" TEXT="T&#x1ea1;o key cho project"/>
+</node>
+<node CREATED="1565236909145" ID="ID_1829299941" MODIFIED="1565236915116" TEXT="php artisan serve">
+<node CREATED="1565236915658" ID="ID_844039753" MODIFIED="1565236919836" TEXT="ch&#x1ea1;y server"/>
+</node>
+</node>
+</node>
+</map>
